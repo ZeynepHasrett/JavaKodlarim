a70a503b91da653ac47dfc21cb3bd47926e1f137
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-09-16 17:45:16 +0300

    Update C01_HelloWorld.java

--- day01_variableOlusturmaVeKullanma/C01_HelloWorld.java
+++ day01_variableOlusturmaVeKullanma/C01_HelloWorld.java
@@ -7,10 +7,5 @@ public class C01_HelloWorld {
         // Java buradaki yazilari yorum olarak kabul eder ve bu yazilari kod gibi calistirmaz
         System.out.println("Hello World");
         System.out.println("Java'nin ilk gununden merhaba...");
-
-        /*
-
-         */
     }
-
 }
