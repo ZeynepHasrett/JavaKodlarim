efa250ae37a1629c2f026758c53e26fbc6654591
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-09-20 18:52:45 +0300

    Ilk Java Projesi (05)

---
+++ EXERCISES/E03_Scanner.java
@@ -0,0 +1,29 @@
+package EXERCISES;
+
+import java.util.Scanner;
+
+public class E03_Scanner {
+
+    public static void main(String[] args) {
+
+        /*
+            SORU: Kullanicidan bir dikdortgenin 2 kenar uzunlugunu alip, dikdortgenin alanini
+                  yazdirin.
+         */
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lutfen dikdortgenin iki kenar uzunluklarini giriniz");
+
+        System.out.print("1. kenar: ");
+
+        double kenar1 = scan.nextDouble();
+
+        System.out.print("2. kenar: ");
+
+        double kenar2 = scan.nextDouble();
+
+        System.out.println(kenar1 * kenar2);
+
+    }
+}
---
+++ EXERCISES/E04_Scanner.java
@@ -0,0 +1,30 @@
+package EXERCISES;
+
+import java.util.Scanner;
+
+public class E04_Scanner {
+
+    public static void main(String[] args) {
+
+        /*
+            SORU: Kullanıcıdan dik kenarlarının uzunluğunu alan ve üçgene ait Hipotenüsü hesaplayan programı yazın.
+         */
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lutfen ucgene ait dik kenar uzunluklarini giriniz");
+
+        System.out.print("1. dik kenar: ");
+
+        double dikKenar1 = scan.nextDouble();
+
+        System.out.print("2. dik kenar: ");
+
+        double dikKenar2 = scan.nextDouble();
+
+        double hipotenus = Math.sqrt(dikKenar1 * dikKenar1 + dikKenar2 * dikKenar2);
+
+        System.out.println("Ucgenin hipotenusu: " + hipotenus);
+
+    }
+}
