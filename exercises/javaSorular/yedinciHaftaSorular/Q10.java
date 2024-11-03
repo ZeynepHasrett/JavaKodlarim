ac7a046410b523e635e1740f3783e900be5b1053
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-11-03 17:31:01 +0300

    Ilk Java Projesi (28)

--- exercises/javaSorular/yedinciHaftaSorular/Q10.java
+++ exercises/javaSorular/yedinciHaftaSorular/Q10.java
@@ -21,50 +21,22 @@ public class Q10 {
             nine hundred nine
          */
 
-        System.out.println(numToEng(101));
+        System.out.println(numToEng(0));
+        System.out.println(numToEng(18));
+        System.out.println(numToEng(126));
+        System.out.println(numToEng(909));
 
     }
 
     public static String numToEng(int sayi) {
 
         int birlerBasamagi = sayi % 10;
+        int onlarBasamagi = (sayi / 10) % 10;
+        int yuzlerBasamagi = sayi / 100;
 
         String metin = "";
 
         switch (sayi) {
-            case 0:
-                metin = "zero";
-                break;
-            case 1:
-                metin = "one";
-                break;
-            case 2:
-                metin = "two";
-                break;
-            case 3:
-                metin = "three";
-                break;
-            case 4:
-                metin = "four";
-                break;
-            case 5:
-                metin = "five";
-                break;
-            case 6:
-                metin = "six";
-                break;
-            case 7:
-                metin = "seven";
-                break;
-            case 8:
-                metin = "eight";
-                break;
-            case 9:
-                metin = "nine";
-                break;
-        }
-
-        switch (sayi){
             case 10:
                 metin = "ten";
                 break;
@@ -84,33 +56,119 @@ public class Q10 {
                 metin = "fifteen";
                 break;
             case 16:
-                metin = "sixteen";
+                metin += "sixteen";
                 break;
             case 17:
-                metin = "seventeen";
+                metin += "seventeen";
                 break;
             case 18:
                 metin = "eighteen";
                 break;
             case 19:
-                metin = "nineteen";
+                metin += "nineteen";
                 break;
             case 20:
-                metin = "twenty ";
+                metin += "twenty";
+        }
+
+        switch (yuzlerBasamagi) {
+
+            case 1:
+                metin += "one hundred ";
+                break;
+            case 2:
+                metin += "two hundred ";
+                break;
+            case 3:
+                metin += "three hundred ";
+                break;
+            case 4:
+                metin += "four hundred ";
+                break;
+            case 5:
+                metin += "five hundred ";
+                break;
+            case 6:
+                metin += "six hundred ";
+                break;
+            case 7:
+                metin += "seven hundred ";
+                break;
+            case 8:
+                metin += "eight hundred ";
+                break;
+            case 9:
+                metin += "nine hundred ";
                 break;
         }
 
-        switch (sayi){
-            case 100:
-                System.out.println("one hundred ");
+        switch (onlarBasamagi) {
+            case 2:
+                metin += "twenty ";
+                break;
+            case 3:
+                metin += "thirty ";
+                break;
+            case 4:
+                metin += "forty ";
+                break;
+            case 5:
+                metin += "fifty ";
+                break;
+            case 6:
+                metin += "sixty ";
+                break;
+            case 7:
+                metin += "seventy ";
+                break;
+            case 8:
+                metin += "eighty ";
+                break;
+            case 9:
+                metin += "ninety ";
                 break;
         }
 
-
+        switch (birlerBasamagi) {
+            case 0:
+                metin += "zero";
+                break;
+            case 1:
+                metin += "one";
+                break;
+            case 2:
+                metin += "two";
+                break;
+            case 3:
+                metin += "three";
+                break;
+            case 4:
+                metin += "four";
+                break;
+            case 5:
+                metin += "five";
+                break;
+            case 6:
+                metin += "six";
+                break;
+            case 7:
+                metin += "seven";
+                break;
+            case 8:
+                metin += "eight";
+                break;
+            case 9:
+                metin += "nine";
+                break;
+        }
 
         return metin;
 
     }
 
 }
+
+
+
+
 
