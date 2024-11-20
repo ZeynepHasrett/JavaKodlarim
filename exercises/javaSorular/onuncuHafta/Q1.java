024d9b6f8777ef30d9c25990cafc6156fe230ae2
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-11-20 19:53:08 +0300

    Ilk Java Projesi (practice)

---
+++ exercises/javaSorular/onuncuHafta/Q1.java
@@ -0,0 +1,20 @@
+package exercises.javaSorular.onuncuHafta;
+
+public class Q1 {
+
+    /*
+        HashSet ile list oluşturalım ve  TreeSet ile sıralayalım
+        ve listeyi yazdıralım.
+
+        HashSet List: sari,mavi,kirmizi,yesil,mor
+
+        Beklenen Çıktı:
+        TreeSet elements:
+        kirmizi
+        mavi
+        mor
+        sari
+        yesil
+     */
+
+}
